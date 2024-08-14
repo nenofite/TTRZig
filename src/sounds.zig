769feb657d1c573972836f81ce5e7091fcc99919
Benjamin Tucker <benjamin.tucker@hey.com>
2024-08-13 22:06:19 -0700

    clean up a bit

--- src/sounds.zig
+++ src/sounds.zig
@@ -1,9 +1,6 @@
 const std = @import("std");
 const p = @import("global_playdate.zig");
 
-pub var bad_hurt: *p.SamplePlayer = undefined;
-pub var good_hurt: *p.SamplePlayer = undefined;
-pub var good_parry: *p.SamplePlayer = undefined;
 pub var bad_parry: *p.SamplePlayer = undefined;
 
 fn make(path: [*c]const u8) !*p.SamplePlayer {
@@ -26,9 +23,6 @@ fn metaMake(comptime name: []const u8) *p.SamplePlayer {
 }
 
 pub fn init() void {
-    bad_hurt = metaMake("bad_hurt");
-    good_hurt = metaMake("good_hurt");
-    good_parry = metaMake("good_parry");
     bad_parry = metaMake("bad_parry");
 }
 
