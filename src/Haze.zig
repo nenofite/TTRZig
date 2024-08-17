b8b956fff2d7dd3b4c4f8e4348279f4a1dc8b36b
Benjamin Tucker <benjamin.tucker@hey.com>
2024-08-16 23:06:46 -0700

    flicker

--- src/Haze.zig
+++ src/Haze.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const p = @import("global_playdate.zig");
 const tween = @import("tween.zig");
 const pat = @import("pattern.zig");
+const perlin = @import("perlin.zig");
 
 const SpriteArena = @import("sprite_arena.zig").SpriteArena;
 
@@ -11,16 +12,18 @@ const bubble1 = &([8]u8{ 0xFF, 0xDD, 0xFF, 0x77, 0xFF, 0xDD, 0xFF, 0x77 } ++ pat
 const bubble2 = &([8]u8{ 0xFF, 0xDD, 0xFF, 0xFF, 0xFF, 0xDD, 0xFF, 0xFF } ++ pat.alphaMask);
 const bubble3 = &([8]u8{ 0xFF, 0xDF, 0xFF, 0xFF, 0xFF, 0xFD, 0xFF, 0xFF } ++ pat.alphaMask);
 
+const baseRadius = 100;
+
 fn drawBubble(bx: i32, by: i32, r: i32) void {
     if (r > 0) {
         const r1 = r;
         const r2 = @max(0, r - 8);
         const r3 = @max(0, r - 16);
         const r4 = @max(0, r - 24);
-        p.playdate.graphics.fillEllipse(bx, by, r1, r1, 0, 360, @intFromPtr(bubble1));
-        p.playdate.graphics.fillEllipse(bx, by, r2, r2, 0, 360, @intFromPtr(bubble2));
-        p.playdate.graphics.fillEllipse(bx, by, r3, r3, 0, 360, @intFromPtr(bubble3));
-        p.playdate.graphics.fillEllipse(bx, by, r4, r4, 0, 360, @intFromEnum(p.LCDSolidColor.ColorWhite));
+        p.playdate.graphics.fillEllipse(bx - r1, by - r1, r1 * 2, r1 * 2, 0, 360, @intFromPtr(bubble1));
+        p.playdate.graphics.fillEllipse(bx - r2, by - r2, r2 * 2, r2 * 2, 0, 360, @intFromPtr(bubble2));
+        p.playdate.graphics.fillEllipse(bx - r3, by - r3, r3 * 2, r3 * 2, 0, 360, @intFromPtr(bubble3));
+        p.playdate.graphics.fillEllipse(bx - r4, by - r4, r4 * 2, r4 * 2, 0, 360, @intFromEnum(p.LCDSolidColor.ColorWhite));
     }
     if (r < 20) {
         p.playdate.graphics.fillEllipse(bx, by, 20, 20, 0, 360, @intFromEnum(p.LCDSolidColor.ColorWhite));
@@ -43,6 +46,7 @@ pub fn init(arena: *SpriteArena) !@This() {
     p.playdate.sprite.setImage(sprite, spriteImg, .BitmapUnflipped);
     p.playdate.sprite.setCenter(sprite, 0, 0);
     p.playdate.sprite.setZIndex(sprite, 5);
+    p.playdate.sprite.setOpaque(sprite, 0);
     p.playdate.sprite.setIgnoresDrawOffset(sprite, 1);
     _ = p.playdate.sprite.setDrawMode(sprite, .DrawModeWhiteTransparent);
     // p.playdate.sprite.setDrawFunction(sprite, drawSprite);
@@ -61,16 +65,20 @@ pub fn deinit(self: *@This()) void {
 }
 
 pub fn update(self: *@This()) void {
-    self.drawImage();
+    const t = p.playdate.system.getCurrentTimeMilliseconds();
+    const noiseOffset: f32 = @as(f32, @floatFromInt(t)) / 500;
+    const flicker = perlin.noise(f32, .{ .x = noiseOffset }) * 0.1 + 0.9;
+    const r = @as(i32, @intFromFloat(baseRadius * flicker));
+    self.drawImage(r);
     p.playdate.sprite.markDirty(self.sprite);
 }
 
-fn drawImage(self: *@This()) void {
+fn drawImage(self: *@This(), r: i32) void {
     p.playdate.graphics.pushContext(self.spriteImg);
     defer p.playdate.graphics.popContext();
 
     p.playdate.graphics.fillRect(0, 0, p.WIDTH, p.HEIGHT, @intFromPtr(haze));
-    drawBubble(p.WIDTH / 2, p.HEIGHT / 2, 50);
+    drawBubble(p.WIDTH / 2, p.HEIGHT / 2, r);
 }
 
 // fn drawSprite(sprite: ?*p.LCDSprite, bounds: p.PDRect, drawrect: p.PDRect) callconv(.C) void {
