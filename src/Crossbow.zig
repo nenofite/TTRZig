92f0f0aef90a2621c1b651546ab9ff21cd22eae3
Benjamin Tucker <benjamin.tucker@hey.com>
2024-09-01 00:22:50 -0700

    tween crossbow

--- src/Crossbow.zig
+++ src/Crossbow.zig
@@ -12,9 +12,10 @@ const Crossbow = @This();
 
 parent: *SpriteArena,
 sprite: *p.LCDSprite,
-untilNextShot: u8 = shotTiming,
+tweens: tween.List,
 
-const shotTiming: u8 = 2 * tween.framerate;
+shouldFire: bool = false,
+nextImage: ?*p.LCDBitmap = null,
 
 pub const Outcome = enum {
     none,
@@ -31,7 +32,11 @@ pub fn init(parent: *SpriteArena, x: f32, y: f32) !*Crossbow {
     self.* = .{
         .parent = parent,
         .sprite = sprite,
+        .tweens = undefined,
     };
+
+    self.tweens = try tween.List.init(parent.alloc);
+    errdefer self.tweens.deinit();
 
     // p.playdate.sprite.setCenter(sprite, 1, 0);
     const img = p.playdate.graphics.getTableBitmap(images.cannonTable, 4) orelse unreachable;
@@ -51,11 +56,37 @@ pub fn deinit(self: *Crossbow) void {
 }
 
 pub fn update(self: *Crossbow) Outcome {
-    self.untilNextShot -= 1;
-    if (self.untilNextShot == 0) {
-        self.untilNextShot = shotTiming;
+    const active = self.tweens.update();
+    if (!active) self.tweenShot();
+
+    if (self.nextImage) |img| {
+        p.playdate.sprite.setImage(self.sprite, img, .BitmapUnflipped);
+        self.nextImage = null;
+    }
+
+    if (self.shouldFire) {
+        self.shouldFire = false;
         return .shoot;
     } else {
         return .none;
     }
+}
+
+fn tweenShot(self: *Crossbow) void {
+    const main = p.playdate.graphics.getTableBitmap(images.cannonTable, 4) orelse unreachable;
+    const draw = p.playdate.graphics.getTableBitmap(images.cannonTable, 5) orelse unreachable;
+    const shoot = p.playdate.graphics.getTableBitmap(images.cannonTable, 6) orelse unreachable;
+
+    var b = self.tweens.build();
+    b.of_discrete(?*p.LCDBitmap, &self.nextImage, main, 0);
+    b.wait(1500);
+
+    b.of_discrete(?*p.LCDBitmap, &self.nextImage, draw, 0);
+    b.wait(500);
+
+    b.of_discrete(?*p.LCDBitmap, &self.nextImage, shoot, 0);
+    b.of_discrete(bool, &self.shouldFire, true, 0);
+    b.wait(200);
+
+    b.of_discrete(?*p.LCDBitmap, &self.nextImage, main, 0);
 }
